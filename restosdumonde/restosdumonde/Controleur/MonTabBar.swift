90e050bde831da3a8fe9acdfebd492dcfd9b06e6
diefthyntis <diefthyntis@gmail.com>
2018-10-02 18:55:08 +0200

    Ajout de matabbar et fichier de constantes

---
+++ restosdumonde/restosdumonde/Controleur/MonTabBar.swift
@@ -0,0 +1,22 @@
+//
+//  MonTabBar.swift
+//  restosdumonde
+//
+//  Created by alan on 02/10/2018.
+//  Copyright © 2018 diefthyntis. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ restosdumonde/restosdumonde/Utilitaire/Constante.swift
@@ -0,0 +1,14 @@
+//
+//  Constante.swift
+//  restosdumonde
+//
+//  Created by alan on 02/10/2018.
+//  Copyright © 2018 diefthyntis. All rights reserved.
+//
+
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(displayP3Red: 30/255, green: 30/255, blue: 30/255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(displayP3Red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
+
