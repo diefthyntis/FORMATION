0fb4f18ea870c2d4feeb3df1de2dd8c54b9528b9
diefthyntis <diefthyntis@gmail.com>
2018-10-02 19:34:58 +0200

    Construction des cellules UICollectionView

--- restosdumonde/restosdumonde/Controleur/MenuController.swift
+++ restosdumonde/restosdumonde/Controleur/MenuController.swift
@@ -8,23 +8,54 @@
 
 import UIKit
 
-class MenuController: UIViewController {
+class MenuController: UIViewController,UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
+    let celluleId = "MenuCelluleId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Les menus du monde"
+        maCollectionView.delegate = self
+        maCollectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var maCollectionView: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
     }
-    */
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cellule = maCollectionView.dequeueReusableCell(withReuseIdentifier: celluleId, for: indexPath) as? MenuCellule {
+            cellule.backgroundColor = GRIS_TRES_FONCE
+            return cellule
+        }
+        return UICollectionViewCell()
+        
+    }
 
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let largeur = maCollectionView.frame.width / 2 - 20
+        return CGSize(width: largeur, height: 200)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        // espace entre deux lignes
+        return 25
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        // espace entre les cellules
+        return 10
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
---
+++ restosdumonde/restosdumonde/Vue/MenuCellule.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCellule.swift
+//  restosdumonde
+//
+//  Created by alan on 02/10/2018.
+//  Copyright © 2018 diefthyntis. All rights reserved.
+//
+
+import UIKit
+
+class MenuCellule: UICollectionViewCell {
+    
+}
